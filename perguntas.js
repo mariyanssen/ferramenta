40f06b2010d287f71398ba3d656619ab2dacc146
mariyanssen <00001085223152sp@al.educacao.sp.gov.br>
2024-11-06 08:24:57 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'quimica',
+    'qual a formula da agua?',
+    'h2o'
+)
+
+criaCartao(
+    'fisica',
+    'quais as escalas termometricas?',
+    'celsius fahrenheit e kelvin '
+)
+
+criaCartao(
+    'biologia',
+    'quantos reinos tem na biologia ',
+    ' 5, sendo animal, vegetal, fungi, protista e monera.'
+)
+
+criaCartao(
+    'matematica',
+    'o que é uma função',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
